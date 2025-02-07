865df5ecfc7101405f3f9dc65ee82a2126469c56
Rafee <rafeehasan88@gmail.com>
2025-02-07 21:16:22 +0600

    first commit

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h4 className='text-center'>Homeeeeeeeeeeeee</h4>
+        </div>
+    );
+};
+
+export default Home;
